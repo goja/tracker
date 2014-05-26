6b16429e0be0fd9143cc425ff4a372e160f5d0f1
Raoul Ghosh <raoul_ghosh@campuseai.org>
2014-05-26 15:42:52 -0400

    added cacheing with ehcache

--- src/main/java/org/goja/tracker/controller/UserController.java
+++ src/main/java/org/goja/tracker/controller/UserController.java
@@ -26,6 +26,7 @@ public class UserController {
 
 	@RequestMapping(value = "/findAll", method = RequestMethod.GET)
 	public String findAll(Map<String, Object> model) {
+		logger.info("*********** delegating to service");
 		model.put("users", userService.findAll());
 		model.put("user", new User());
 		return "user";
--- src/main/java/org/goja/tracker/service/impl/UserServiceImpl.java
+++ src/main/java/org/goja/tracker/service/impl/UserServiceImpl.java
@@ -4,29 +4,40 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.apache.log4j.Logger;
 import org.goja.tracker.model.User;
 import org.goja.tracker.repository.UserRepository;
 import org.goja.tracker.service.UserService;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+	protected static final Logger logger = Logger.getLogger(UserServiceImpl.class);
+
 	@Inject
 	private UserRepository userRepository;
 
+	@Cacheable("tracker")
 	public List<User> findAll() {
+		logger.info("******************* hitting database now");
 		return userRepository.findAll();
 	}
 
+	@CacheEvict(value = "tracker", allEntries = true)
 	public void save(User user) {
+		logger.info("******************* clearing cache");
 		userRepository.saveAndFlush(user);
 	}
 
+	@CacheEvict(value = "tracker", allEntries = true)
 	public void delete(Long id) {
 		userRepository.delete(id);
 	}
 
+	@CacheEvict(value = "tracker", allEntries = true)
 	public void deleteAll() {
 		userRepository.deleteAll();
 	}
